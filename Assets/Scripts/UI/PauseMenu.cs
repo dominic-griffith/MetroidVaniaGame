b6c53ecad05f26b22e7f047ef16188f3f35f06a5
Dominic Griffith <dgriffith6227@sdsu.edu>
2022-08-28 19:38:18 -0700

    Pause Menu UI

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+    private void Update()
+    {
+        GetUserInput();
+    }
+
+    private void GetUserInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneLoader.GetInstance().LoadSceneByName("Main Menu");
+    }
+
+    public void ExitGame()
+    {
+        SceneLoader.GetInstance().ExitGame();
+    }
+}
